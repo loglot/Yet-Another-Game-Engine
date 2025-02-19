a769918d8ef8b5d169e147ff1b2fd4d48f8a7377
loglot <88983354+loglot@users.noreply.github.com>
2025-02-19 12:41:02 -0600

    some bug fixes

--- game/main.js
+++ game/main.js
@@ -1,3 +1,10 @@
+// Basic Error Handler
+
+// window.onerror = function(msg, url, linenumber) {
+//     alert('Error message: '+msg+'\nURL: '+url+'\nLine Number: '+linenumber);
+//     return true;
+// }
+
 export class Main{
     game
     constructor(Game){
